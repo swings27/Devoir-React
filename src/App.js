0fb575b186beaa66f2cd9c62c9593ed309e1e5fd
swings27 <anna.trbd@gmail.com>
2025-05-15 14:19:44 +0200

    Browser with routes in place, pages created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Nav from './components/nav';
 import Footer from './components/footer';
 import Home from './pages/home';
+import Services from './pages/services';
+import Portfolio from './pages/portfolio';
+import Contact from './pages/contact';
+import Legals from './pages/legals';
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
       <Nav />
       <Routes>
         <Route path ="/" element={<Home />} />
+        <Route path ="/services" element={<Services />} />
+        <Route path ="/portfolio" element={<Portfolio />} />
+        <Route path ="/contact" element={<Contact />} />
+        <Route path ="/mentionslegales" element={<Legals />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/legals.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Legals() {
+    return (
+        <>
+            <h1>Mention légales</h1>
+        </>
+    )
+};
