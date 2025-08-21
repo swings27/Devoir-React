1e04a83ee52a04214b0014cbdfe69fabf2bdd0c9
swings27 <anna.trbd@gmail.com>
2025-08-21 10:38:56 +0200

    page services done

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 const Header = (props) => {
     return (
-        <>
-            <img src={props.source} alt={props.alt} />
+        <div>
+            <img src={props.source} alt={props.alt} style={{ width: "100%"}} />
+            <div className="pt-4 mx-auto text-center" style={{ width: "500px"}}>
             <h1>{props.title}</h1>
             <p>{props.description}</p>
-            <hr />
-        </>
+            <hr className="border border-primary border-3 opacity-75 mb-5" />
+            </div>
+        </div>
     )
 };
 
--- src/pages/services.js
+++ src/pages/services.js
@@ -5,10 +5,12 @@ export default function Services() {
 
     const Offer = (props) => {
         return (
-            <div>
-                <img src={props.source} alt={props.alternative} />
-                <h2>{props.name}</h2>
-                <p>{props.description}</p>
+            <div className="card my-3 text-center pt-4" style={{ width: "20vw"}}>
+                <i className={props.icon} style={{ fontSize: "2em" }}/>
+                <div className="card-body">
+                <h4 className="card-title fw-bold">{props.name}</h4>
+                <p className="card-text">{props.description}</p>
+                </div>
             </div>
         );
     };
@@ -18,13 +20,13 @@ export default function Services() {
             <div>
                 <Header source="./banner.jpg" alt="image graphique bleue" title="Mon offre de services" description="Voici les prestations sur lesquelles je peux intervenir" />
             </div>
-            <div className="services">
-                <Offer source="" alternative="logo pinceau" name="UX Design" description="L'UX design est une discipline qui consiste à concevoir des produits (sites web,
+            <div className="d-flex flex-row mb-5 justify-content-center gap-3">
+                <Offer icon="bi bi-brush text-primary" name="UX Design" description="L'UX design est une discipline qui consiste à concevoir des produits (sites web,
                  applications mobiles, logiciels, objets connectés,etc.) en plaçant l'utilisateur au centre des préoccupations. L'objectif est 
                  de rendre l'expérience utilisateur la plus fluide et agréable possible." />
-                <Offer source="" alternative="balise fermeture" name="Développement web" description="Le développement web de sites web consiste à créer des sites internet 
+                <Offer icon="bi bi-code-slash text-primary" name="Développement web" description="Le développement web de sites web consiste à créer des sites internet 
                 en utilisant des langages de programmation (HTML, CSS, JavaScript, PHP, etc.) et des frameworks (Bootstrap, React, Angular, etc.)." />
-                <Offer source="" alternative="loupe" name="Référencement" description="Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web 
+                <Offer icon="bi bi-search text-primary" name="Référencement" description="Le référencement naturel (SEO) est une technique qui consiste à optimiser un site web 
                 pour le faire remonter dans les résultats des moteurs de recherche (Google, Bing, Yahoo, etc.). L'objectif est d'attirer un maximum 
                 de visiteurs qualifiés sur le site." />
             </div>
